ecea1b52dd69b3c9447e1eee717121ed38943ac4
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-08-28 02:17:14 +0800

    update basic.lua, remove few keybindings

--- lua/basics.lua
+++ lua/basics.lua
@@ -15,16 +15,15 @@ vim.o.scrolloff = 3
 vim.o.errorbells = false
 vim.o.shiftwidth = 2
 vim.o.numberwidth = 6
-vim.o.termguicolors = true 
+vim.o.termguicolors = true
 vim.o.colorcolumn = '80'
 vim.o.encoding = 'UTF-8'
 vim.o.showmode = true
-vim.o.showtabline = 2
+vim.o.showtabline = 4
 vim.o.signcolumn = 'yes'
 vim.o.mouse = 'a'
 vim.o.clipboard = 'unnamedplus'
 vim.o.t_Co = 256
-
 
 vim.api.nvim_set_keymap('n', 'vs', ':vs<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', 'sp', ':sp<CR>', { noremap = true })
@@ -39,42 +38,8 @@ vim.api.nvim_set_keymap('n', 'to', ':tabo<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-S>', ':%s/', { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>t", ":sp<CR> :term<CR> :resize 20N<CR> i", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {})
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {}) -- to esc with jk 
 
--- vim.g["netrw_banner"] = 0
--- vim.g["netrw_liststyle"] = 3
--- vim.g["netrw_winsize"] = 25
-
-vim.cmd([[
-  function! s:check_back_space() abort
-    let col = col('.') - 1
-    return !col || getline('.')[col - 1]  =~ '\s'
-  endfunction
-
-  " Insert <tab> when previous text is space, refresh completion if not.
-  inoremap <silent><expr> <TAB>
-	\ coc#pum#visible() ? coc#pum#next(1):
-	\ <SID>check_back_space() ? "\<Tab>" :
-	\ coc#refresh()
-  inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
-]])
-
--- vim.cmd([[
---   hi ActiveWindow guibg=#1B1B26
---   hi InactiveWindow guibg=#2d2d3b
-
---   augroup WindowManagement
---     autocmd!
---     autocmd WinEnter * call Handle_Win_Enter()
---   augroup END
-
---   function! Handle_Win_Enter()
---     setlocal winhighlight=Normal:ActiveWindow,NormalNC:InactiveWindow
---     highlight SignColumn guibg=Normal:ActiveWindow,NormalNC:InactiveWindow
---   endfunction
-
---   highlight CursorLine cterm=NONE ctermfg=NONE ctermbg=233 guifg=NONE guibg=#121212
---   autocmd InsertEnter * highlight CursorLine cterm=NONE ctermfg=NONE ctermbg=234 guifg=NONE guibg=#1c1c1c
---   autocmd InsertLeave * highlight CursorLine cterm=NONE ctermfg=NONE ctermbg=233 guifg=NONE guibg=#121212
--- ]])
-
+vim.g["netrw_banner"] = 0
+vim.g["netrw_liststyle"] = 3
+vim.g["netrw_winsize"] = 25
