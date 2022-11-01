a844b78a4b7104c22a87bdcaa2475a2d9a9fea85
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-11-01 10:35:17 +0800

    feat: nvim-tree dotfiles, useful for .env .gitignore etc

--- lua/nvim-tree-config.lua
+++ lua/nvim-tree-config.lua
@@ -1,18 +1,18 @@
 require("nvim-tree").setup({
     sort_by = "case_sensitive",
     view = {
-      adaptive_size = true,
-      mappings = {
-        list = {
-          { key = "u", action = "dir_up" },
+        adaptive_size = true,
+        mappings = {
+            list = {
+                { key = "u", action = "dir_up" },
+            },
         },
-      },
     },
     renderer = {
-      group_empty = true,
+        group_empty = true,
     },
     filters = {
-      dotfiles = true,
+        dotfiles = false,
     },
 })
 vim.api.nvim_set_keymap('n', '<C-N>', ":NvimTreeToggle<CR>", { noremap = true }) -- remap Ctrl+n for using the mapping for nvim-tree
