35acc2f5c648f64e6886985c82a638c1245ac4fa
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-11-07 16:30:14 +0800

    add config for nvim-tree-config to see the hidden files and change the symbol

--- lua/nvim-tree-config.lua
+++ lua/nvim-tree-config.lua
@@ -1,7 +1,9 @@
 require("nvim-tree").setup({
     sort_by = "case_sensitive",
     view = {
+        relativenumber = true,
         adaptive_size = true,
+        side = "right",
         mappings = {
             list = {
                 { key = "u", action = "dir_up" },
@@ -10,9 +12,48 @@ require("nvim-tree").setup({
     },
     renderer = {
         group_empty = true,
+        icons = {
+            webdev_colors = true,
+            git_placement = "before",
+            padding = " ",
+            symlink_arrow = " ➛ ",
+            show = {
+              file = true,
+              folder = true,
+              folder_arrow = true,
+              git = true,
+            },
+            glyphs = {
+              default = "",
+              symlink = "",
+              bookmark = "",
+              folder = {
+                arrow_closed = "",
+                arrow_open = "",
+                default = "",
+                open = "",
+                empty = "",
+                empty_open = "",
+                symlink = "",
+                symlink_open = "",
+              },
+              git = {
+                unstaged = "",
+                staged = "✓",
+                unmerged = "",
+                renamed = "",
+                untracked = "+",
+                deleted = "",
+                ignored = "",
+              },
+            },
+        },
     },
     filters = {
         dotfiles = false,
+        exclude = {
+            ".*"
+        },
     },
 })
 vim.api.nvim_set_keymap('n', '<C-N>', ":NvimTreeToggle<CR>", { noremap = true }) -- remap Ctrl+n for using the mapping for nvim-tree
