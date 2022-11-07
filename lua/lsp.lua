6df22844b141b837c8fe406b7d1c37279096c73e
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-11-07 16:29:43 +0800

    reconfigure lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -10,8 +10,7 @@ lspconfig.util.default_config = vim.tbl_extend(
     "force",
     lspconfig.util.default_config,
     {
-        on_attach = on_attach,
-        handlers = handlers
+        handlers = Handlers
     }
 )
 
@@ -32,108 +31,23 @@ lspconfig.sumneko_lua.setup {
 }
 
 -- 5. auto complete colorschemes border
---vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
-vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
+ -- vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
 
-local border = {
-    { "🭽", "FloatBorder" },
-    { "▔", "FloatBorder" },
-    { "🭾", "FloatBorder" },
-    { "▕", "FloatBorder" },
-    { "🭿", "FloatBorder" },
-    { "▁", "FloatBorder" },
-    { "🭼", "FloatBorder" },
-    { "▏", "FloatBorder" },
-}
+ local border = {
+     { "🭽", "FloatBorder" },
+     { "▔", "FloatBorder" },
+     { "🭾", "FloatBorder" },
+     { "▕", "FloatBorder" },
+     { "🭿", "FloatBorder" },
+     { "▁", "FloatBorder" },
+     { "🭼", "FloatBorder" },
+     { "▏", "FloatBorder" },
+ }
 
--- LSP settings (for overriding per client)
-local handlers = {
+-- -- LSP settings (for overriding per client)
+Handlers = {
     ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
     ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
 }
 
--- To instead override globally
-local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-    opts = opts or {}
-    opts.border = opts.border or border
-    return orig_util_open_floating_preview(contents, syntax, opts, ...)
-end
-
--- getting the completion kinds files from ./lua/lsp-completion-kinds
--- require('lsp-completion-kinds')
-
-vim.diagnostic.config({
-    virtual_text = true,
-    signs = true,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = false,
-})
-
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
-function PrintDiagnostics(opts, bufnr, line_nr, client_id)
-    bufnr = bufnr or 0
-    line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
-    opts = opts or { ['lnum'] = line_nr }
-
-    local line_diagnostics = vim.diagnostic.get(bufnr, opts)
-    if vim.tbl_isempty(line_diagnostics) then return end
-
-    local diagnostic_message = ""
-    for i, diagnostic in ipairs(line_diagnostics) do
-        diagnostic_message = diagnostic_message .. string.format("%d: %s", i, diagnostic.message or "")
-        print(diagnostic_message)
-        if i ~= #line_diagnostics then
-            diagnostic_message = diagnostic_message .. "\n"
-        end
-    end
-    vim.api.nvim_echo({ { diagnostic_message, "Normal" } }, false, {})
-end
-
-vim.cmd [[ autocmd! CursorHold * lua PrintDiagnostics() ]]
-
--- You will likely want to reduce updatetime which affects CursorHold
--- note: this setting is global and should be set only once
-vim.o.updatetime = 250
-
--- Go To Definition in a split Window
-vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
-local function goto_definition(split_cmd)
-    local util = vim.lsp.util
-    local log = require("vim.lsp.log")
-    local api = vim.api
-
-    -- note, this handler style is for neovim 0.5.1/0.6, if on 0.5, call with function(_, method, result)
-    local handler = function(_, result, ctx)
-        if result == nil or vim.tbl_isempty(result) then
-            local _ = log.info() and log.info(ctx.method, "No location found")
-            return nil
-        end
-
-        if split_cmd then
-            vim.cmd(split_cmd)
-        end
-
-        if vim.tbl_islist(result) then
-            util.jump_to_location(result[1])
-
-            if #result > 1 then
-                util.set_qflist(util.locations_to_items(result))
-                api.nvim_command("copen")
-                api.nvim_command("wincmd p")
-            end
-        else
-            util.jump_to_location(result)
-        end
-    end
-
-    return handler
-end
-
-vim.lsp.handlers["textDocument/definition"] = goto_definition('split')
