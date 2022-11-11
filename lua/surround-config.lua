6a82dc6e6bf8ba29c84e71f1e504d5c55914c5a2
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-11-11 19:09:59 +0800

    add surroound configuration

---
+++ lua/surround-config.lua
@@ -0,0 +1,16 @@
+local surround = require('surround')
+
+surround.setup({
+  context_offset = 100,
+  load_autogroups = false,
+  mappings_style = "sandwich",
+  map_insert_mode = true,
+  quotes = {"'", '"'},
+  brackets = {"(", '{', '['},
+  space_on_closing_char = false,
+  pairs = {
+    nestable = { b = { "(", ")" }, s = { "[", "]" }, B = { "{", "}" }, a = { "<", ">" } },
+    linear = { q = { "'", "'" }, t = { "`", "`" }, d = { '"', '"' } }
+  },
+  prefix = "s"
+})
