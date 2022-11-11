ef8732c48da76d0599412597cf28d763b28837ed
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-11-12 00:00:09 +0800

    add more comments in keybindings and add keybindings for lsp usage

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,18 +1,24 @@
 -- custom keybindings
-vim.api.nvim_set_keymap('n', 'vs', ':vs<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'sp', ':sp<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-L>', '<C-W><C-L>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-H>', '<C-W><C-H>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-K>', '<C-W><C-K>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-J>', '<C-W><C-J>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'tn', ':tabnew<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'tk', ':tabnext<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'tj', ':tabprev<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'to', ':tabo<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-S>', ':%s/', { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>t", ":sp<CR> :term<CR> :resize 20N<CR> i", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {}) -- to esc with jk 
+
+local opts = { noremap = true, silent = true } -- options
+
+-- better vim motion
+vim.api.nvim_set_keymap('n', 'vs', ':vs<CR>', opts) -- split the tab verticaly
+vim.api.nvim_set_keymap('n', 'sp', ':sp<CR>', opts) -- split the tab horizontally
+vim.api.nvim_set_keymap('n', 'tn', ':tabnew<CR>', opts) -- navigate to new tab
+vim.api.nvim_set_keymap('n', 'tk', ':tabnext<CR>', opts) -- navigate to next tab
+vim.api.nvim_set_keymap('n', 'tj', ':tabprev<CR>', opts) -- navigate to previous tab
+vim.api.nvim_set_keymap('n', 'to', ':tabo<CR>', opts) -- only one tab
+vim.api.nvim_set_keymap('n', '<C-S>', ':%s/', opts) -- find and replace
+vim.api.nvim_set_keymap("n", "<leader>t", ":sp<CR> :term<CR> :resize 20N<CR> i", opts) -- open terminal
+vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", opts)
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', opts) -- to esc with jk
+
+-- vim keybinding movement through the buffer
+vim.api.nvim_set_keymap('n', '<C-L>', '<C-W><C-L>', opts)
+vim.api.nvim_set_keymap('n', '<C-H>', '<C-W><C-H>', opts)
+vim.api.nvim_set_keymap('n', '<C-K>', '<C-W><C-K>', opts)
+vim.api.nvim_set_keymap('n', '<C-J>', '<C-W><C-J>', opts)
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -24,41 +30,40 @@ local luasnip = require('luasnip')
 -- nvim-cmp keybinding setup
 local cmp = require('cmp')
 cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  }),
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    }),
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+    },
 }
-
--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,6 +1,42 @@
 -- get the plugin
 local lsp_installer = require("nvim-lsp-installer")
 local lspconfig = require("lspconfig")
+local opts = { noremap = true, silent = true } -- options
+
+-- for configuration in the lsp config on line 56
+local on_attach = function(client, bufnr)
+    require('completion').on_attach()
+
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings
+    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts) -- go to declaration
+    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts) -- goo to code definition
+    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+
+    -- Set some keybinds conditional on server capabilities
+    if client.resolved_capabilities.document_formatting then
+        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    elseif client.resolved_capabilities.document_range_formatting then
+        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    end
+end
 
 -- 1. Set up nvim-lsp-installer first!
 lsp_installer.setup {}
@@ -16,7 +52,9 @@ lspconfig.util.default_config = vim.tbl_extend(
 
 -- 3. Loop through all of the installed servers and set it up via lspconfig
 for _, server in ipairs(lsp_installer.get_installed_servers()) do
-    lspconfig[server.name].setup {}
+    lspconfig[server.name].setup {
+        on_attach = on_attach,
+    }
 end
 
 -- 4. for lua configuration define vim variable
@@ -32,22 +70,21 @@ lspconfig.sumneko_lua.setup {
 
 -- 5. auto complete colorschemes border
 vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
- -- vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+-- vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
 
- local border = {
-     { "🭽", "FloatBorder" },
-     { "▔", "FloatBorder" },
-     { "🭾", "FloatBorder" },
-     { "▕", "FloatBorder" },
-     { "🭿", "FloatBorder" },
-     { "▁", "FloatBorder" },
-     { "🭼", "FloatBorder" },
-     { "▏", "FloatBorder" },
- }
+local border = {
+    { "🭽", "FloatBorder" },
+    { "▔", "FloatBorder" },
+    { "🭾", "FloatBorder" },
+    { "▕", "FloatBorder" },
+    { "🭿", "FloatBorder" },
+    { "▁", "FloatBorder" },
+    { "🭼", "FloatBorder" },
+    { "▏", "FloatBorder" },
+}
 
 -- -- LSP settings (for overriding per client)
 Handlers = {
     ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
     ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
 }
-
